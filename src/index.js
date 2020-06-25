29a518eff1f12d6493a38bc90541239beb93fcef
Vivek Vellaiyappan <vivekvellaiyappans@gmail.com>
2020-06-25 18:57:57 -0500

    #2 - eslint setup done. ran into a stupid issue and took longgggg time to resolve. issue mentioned in https://stackoverflow.com/questions/60124662/create-react-app-eslint-error-while-deployment-on-local-machine.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Adapter from './Adapter';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Adapter />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
