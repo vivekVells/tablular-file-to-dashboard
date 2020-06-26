6064e06dcf121454b6c8fac4044047ade080990b
Vivek Vellaiyappan <vivekvellaiyappans@gmail.com>
2020-06-25 19:44:40 -0500

    #2 - typescript setup done. cleaned up few things.

--- src/Adapter.tsx
+++ src/Adapter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Adapter = () => {
+const Adapter: React.FC = () => {
 	return <div>Welcome to File To Dashboard</div>;
 };
 
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Adapter from './Adapter';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Adapter />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
