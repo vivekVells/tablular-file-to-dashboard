5c80a20d18d13409cfe0ed86eb8996d5750fadf9
Vivek Vellaiyappan <vivekvellaiyappans@gmail.com>
2020-06-25 22:31:06 -0500

    #2 - removed the stupid eslint setup. antd setup done. sass setup done

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Adapter from './Adapter';
+import Adapter from './adapter';
+
+// importing antd css file
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
 	<React.StrictMode>
